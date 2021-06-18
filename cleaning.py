245dbd948b5274c234e2d9533a2c14bb9ad276f5
Judhajit-Roy <56783167+Judhajit-Roy@users.noreply.github.com>
2021-06-18 17:27:18 +0530

    Delete cleaning.py

--- cleaning.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import re
-import string
-import data
-
-eng_hin_data = pd.read_table("data/hin.txt",names=['English','Hindi','value']).drop('value',axis=1)
-
-print(eng_hin_data.head())
